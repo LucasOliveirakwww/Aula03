81248da571c0b792ed840f80623734073d5480a0
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-03-31 14:53:27 -0300

    Exercício 02 da aula 03

--- Aula03.py
+++ Aula03.py
@@ -1,6 +1,11 @@
 #Programa para ler nome, idade e salário para mostrar subsequentemente
 nome = input ("Digite seu nome: ")
 idade = int(input("Digite sua idade:"))
-salário = float(input("O seu salário é: "))
+salario = float(input("O seu salario é: "))
 #Espaço amostral
-print(f"Nome do indivíduo é {nome}, idade {idade}, salário {salário}")
+print(f"Nome do indivíduo é {nome}, idade {idade}, salário {salario}")
+#Calcular percentual de aumento
+aumento = float(input("Valor do percentual aumentado: "))
+calculo = salario * aumento / 100
+#Resultado do aumento aditado
+total = print(salario + calculo)
