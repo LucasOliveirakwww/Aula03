14a3ee6a9788ef59bc093167d7f406aa8d0e9b41
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-03-31 16:21:07 -0300

    Exercício 05 da aula 03

---
+++ Exercício 05 da aula 03.py
@@ -0,0 +1,7 @@
+#Receber números e apresentar em ordem crescente
+num1 = int(input("Primeiro número: "))
+num2 = int(input("Segundo número: "))
+if num1 > num2:
+ print (num2,num1)
+else:
+ print (num1,num2)
