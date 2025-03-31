c5453db8e07716681a7851ec94879856c2ccd6f3
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-03-31 16:52:30 -0300

    Exercício 06.3 da aula 03

--- Exercício 06 da aula 03.py
+++ Exercício 06 da aula 03.py
@@ -3,7 +3,10 @@ nota1 = float(input("Insira a primeira nota: "))
 nota2 = float(input("Insira a segunda nota: "))
 nota3 = float(input("Insira a terceira nota: "))
 Media = (nota1+nota2+nota3)/3
-if nota1 >= 7.0:
-    print ("Aprovado")
+if Media >= 7.0:
+    print (f"Aprovado {Media}")
 else:
-    print ("Reprovado")
+    if Media <4:
+        print(f"Reprovado {Media}")
+    else:
+        print(f"Recuperaçao {Media}")
