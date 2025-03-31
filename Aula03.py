802ccd76c67342ca8e328956f372dbcb81ceb266
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-03-31 14:14:42 -0300

    Exercício 01 da aula 03

---
+++ Aula03.py
@@ -0,0 +1,6 @@
+#Programa para ler nome, idade e salário para mostrar subsequentemente
+nome = input ("Digite seu nome: ")
+idade = int(input("Digite sua idade:"))
+salário = float(input("O seu salário é: "))
+#Espaço amostral
+print(f"Nome do indivíduo é {nome}, idade {idade}, salário {salário}")
