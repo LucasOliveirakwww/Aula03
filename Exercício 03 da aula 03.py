7a6f78e98ea16dbd30fcaefc14a6fca4ab594929
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-03-31 15:12:33 -0300

    Exercício 03 da aula 03

---
+++ Exercício 03 da aula 03.py
@@ -0,0 +1,16 @@
+#Ler 2 números e realizar as 4 operações básicas
+numero1 = float(input("Primeiro número: "))
+numero2 = float(input("Segundo número: "))
+#Cálculos
+soma = numero1 + numero2
+subtraçao = numero1 - numero2
+multiplicaçao = numero1 * numero2
+divisao = numero1 / numero2
+#Resultantes
+print(f"soma {numero1} + {numero2} = {soma}")
+print(f"subtraçao {numero1} - {numero2} = {subtraçao}")
+print(f"multiplicaçao {numero1} * {numero2} = {multiplicaçao}")
+print(f"divisao {numero1} / {numero2} = {divisao}")
+
+
+
