889f4278f7e3987cc2d9be34b217c2c421a5acf2
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-04-02 14:45:53 -0300

    Exercício 08 da aula 03

--- Exercício 08 da aula 03.py
+++ Exercício 08 da aula 03.py
@@ -1,17 +1,17 @@
 ##Algoritmo que leia e calcule o número de Litros e o tipo vendido para o cliente
 Quantidade = float(input("Quantidade em litros comprados: "))
 Combustivel = input ("Qual o tipo de combustível \n"
-                     "G para gasolina\n"
-                     "E para Etanol: \n ")
+                     "G or g para gasolina\n"
+                     "E or e para Etanol: \n ")
 vEta = 4.90
 vGas = 5.80
 #Calculo
-if Combustivel == "G":
+if Combustivel == 'G' or Combustivel =='g':
     ValorG = Quantidade*vGas
     print (f"Você abasteceu {Quantidade} litros e gastou {ValorG:.2f}")
 
-elif Combustivel == "E":
-    ValorE = Quantidade * vEta
+elif Combustivel == 'E' or Combustivel == 'e':
+    ValorE = Quantidade*vEta
     print (f"Você abasteceu {Quantidade} litros e gastou {ValorE:.2f}")
 
 
